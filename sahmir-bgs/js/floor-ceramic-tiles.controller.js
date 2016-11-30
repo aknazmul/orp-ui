43e33eeacecea1c41913960352f80c3350a53c4c
unknown <aknazmul@gmail.com>
2016-12-01 01:07:39 +0600

    added fancy box and ready the pdp

--- sahmir-bgs/js/floor-ceramic-tiles.controller.js
+++ sahmir-bgs/js/floor-ceramic-tiles.controller.js
@@ -13,38 +13,57 @@ function ($scope, $document, screenSize) {
     // console.log($scope.isMobile);
 
 
-    $scope.products = [
-        {
-            id: 1,
-            imgUrl: "img/catalogue-01.jpg",
-            name: "Product name 01",
-            pCode: "T154",
-            description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-            productThumbs: [
-                {
-                    id: 1,
-                    imgUrlstyle: "img/catalogue-01.jpg",
-                    name: "Product name 01",
-                    pCode: "T467",
-                    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-                },
-                {
-                    id: 1,
-                    imgUrlstyle: "img/catalogue-02.jpg",
-                    name: "Product name 02",
-                    pCode: "T102",
-                    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy.",
-                },
-                {
-                    id: 1,
-                    imgUrlstyle: "img/catalogue-03.jpg",
-                    name: "Product name 03",
-                    pCode: "T053",
-                    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-                }
-            ]
-        } 
-    ]
+    $scope.products =  [
+                    {
+                        id: 1,
+                        imgUrlstyle: "img/catalogue-01.jpg",
+                        name: "Product name 01",
+                        pCode: "T467",
+                    },
+                    {
+                        id: 2,
+                        imgUrlstyle: "img/catalogue-02.jpg",
+                        name: "Product name 02",
+                        pCode: "T102",
+                    },
+                    {
+                        id: 3,
+                        imgUrlstyle: "img/catalogue-03.jpg",
+                        name: "Product name 03",
+                        pCode: "T053",
+                    },
+                    {
+                        id: 2,
+                        imgUrlstyle: "img/catalogue-02.jpg",
+                        name: "Product name 02",
+                        pCode: "T102",
+                    },
+                    {
+                        id: 5,
+                        imgUrlstyle: "img/catalogue-01.jpg",
+                        name: "Product name 01",
+                        pCode: "T467",
+                    },
+                    {
+                        id: 6,
+                        imgUrlstyle: "img/catalogue-02.jpg",
+                        name: "Product name 02",
+                        pCode: "T102",
+                    },
+                    {
+                        id: 7,
+                        imgUrlstyle: "img/catalogue-03.jpg",
+                        name: "Product name 03",
+                        pCode: "T053",
+                    },
+                    {
+                        id: 8,
+                        imgUrlstyle: "img/catalogue-02.jpg",
+                        name: "Product name 02",
+                        pCode: "T102",
+                    }
+                ]
+        
 
 }]);
 
